64ed59a00174874f58cc008042e293505e922dbf
iobaidat <iobaidat@charlotte.edu>
2025-07-14 21:51:52 -0400

    Fix minor issue in bridge netfilter

--- connections/singleDestroy.sh
+++ connections/singleDestroy.sh
@@ -15,18 +15,11 @@ sudo ip link set dev si-$NAME nomaster
 sudo ip link set si-$NAME down
 sudo ip link delete si-$NAME
 
-# no need for the following
-# sudo ip link set se-$NAME down &>/dev/null
-# sudo ip link delete se-$NAME &>/dev/null
-
-# sudo brctl delif br-$NAME tap-$NAME
-# sudo ifconfig tap-$NAME down
-# sudo tunctl -d tap-$NAME
 sudo ip link set dev tap-$NAME nomaster
 sudo ip link set tap-$NAME down
 sudo ip link delete tap-$NAME
 
-# sudo ifconfig br-$NAME down
-# sudo brctl delbr br-$NAME
 sudo ip link set dev br-$NAME down
 sudo ip link del br-$NAME
+
+sudo modprobe -r br_netfilter
--- connections/singleEndSetup.sh
+++ connections/singleEndSetup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+modprobe br_netfilter
+
 pushd /proc/sys/net/bridge
 for f in bridge-nf-*; do echo 0 > $f; done
 popd
